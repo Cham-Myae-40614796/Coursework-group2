9b701e9ed6217aa770f3209c4068d0f1b615fd3d
thar-htet-nyan <40614795@live.napier.ac.uk>
2023-07-11 16:04:08 +0630

    Created City.java

---
+++ src/main/java/com/napier/coursework/City.java
@@ -0,0 +1,44 @@
+package com.napier.coursework;
+
+public class City {
+
+    private String CityName;
+
+    private String CountryName;
+
+    private String District;
+
+    private double Population;
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public void setCountryName(String countryName) {
+        CountryName = countryName;
+    }
+
+    public void setDistrict(String district) {
+        District = district;
+    }
+
+    public void setPopulation(double population) {
+        Population = population;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public String getCountryName() {
+        return CountryName;
+    }
+
+    public String getDistrict() {
+        return District;
+    }
+
+    public double getPopulation() {
+        return Population;
+    }
+}
---
+++ src/main/java/com/napier/coursework/CityReport.java
@@ -0,0 +1,2 @@
+package com.napier.coursework;public class CityReport {
+}
