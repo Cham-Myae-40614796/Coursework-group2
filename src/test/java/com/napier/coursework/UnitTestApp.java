f8d1214c4969623adbd5a47620c0364022566ae5
thar-htet-nyan <40614795@live.napier.ac.uk>
2023-07-19 22:28:47 +0630

    updated unit testing and integration testing

--- src/test/java/com/napier/coursework/UnitTestApp.java
+++ src/test/java/com/napier/coursework/UnitTestApp.java
@@ -11,12 +11,37 @@ public class UnitTestApp {
 
     static CountryReport cotyr;
 
+    static CityReport cr;
+
+    static CapitalCityReport ccr;
+
+    static Country country;
+
+    static City city;
+
+    static City capitalCity;
+
     @BeforeAll
     static void init(){
         cotyr = new CountryReport();
+        country = new Country();
+        country.setCountryCode("IND");
+        country.setCountryName("India");
+        country.setContinent("Asia");
+        country.setRegion("Southern and Central Asia");
+        country.setPopulation(3287263);
+        country.setCapital("New Delhi");
+
+        city = new City();
+        city.setCityName("Seoul");
+        city.setCountryName("South Korea");
+        city.setDistrict("Seoul");
+        city.setPopulation(9981619);
+
     }
     @Test
     void displayCountryNullTest(){
+
         cotyr.displayCountries(null, "World", "", false);
     }
 
@@ -38,13 +63,6 @@ public class UnitTestApp {
     @Test
     void displayCountryInWorldTest() {
         ArrayList<Country> extractedCountries = new ArrayList<>();
-        Country country = new Country();
-        country.setCountryCode("ABW");
-        country.setCountryName("Aruba");
-        country.setContinent("North America");
-        country.setRegion("Caribbean");
-        country.setPopulation(103000);
-        country.setCapital("Oranjestad");
 
         extractedCountries.add(country);
         cotyr.displayCountries(extractedCountries, "World", "", false);
@@ -53,13 +71,6 @@ public class UnitTestApp {
     @Test
     void displayCountryInContinentTest() {
         ArrayList<Country> extractedCountries = new ArrayList<>();
-        Country country = new Country();
-        country.setCountryCode("ABW");
-        country.setCountryName("Aruba");
-        country.setContinent("North America");
-        country.setRegion("Caribbean");
-        country.setPopulation(103000);
-        country.setCapital("Oranjestad");
 
         extractedCountries.add(country);
         cotyr.displayCountries(extractedCountries, "Continent", "Europe", false);
@@ -68,13 +79,6 @@ public class UnitTestApp {
     @Test
     void displayCountryInRegionTest() {
         ArrayList<Country> extractedCountries = new ArrayList<>();
-        Country country = new Country();
-        country.setCountryCode("ABW");
-        country.setCountryName("Aruba");
-        country.setContinent("North America");
-        country.setRegion("Caribbean");
-        country.setPopulation(103000);
-        country.setCapital("Oranjestad");
 
         extractedCountries.add(country);
         cotyr.displayCountries(extractedCountries, "Region", "Eastern Africa", false);
@@ -83,13 +87,6 @@ public class UnitTestApp {
     @Test
     void displayTopCountryInWorldTest() {
         ArrayList<Country> extractedCountries = new ArrayList<>();
-        Country country = new Country();
-        country.setCountryCode("ABW");
-        country.setCountryName("Aruba");
-        country.setContinent("North America");
-        country.setRegion("Caribbean");
-        country.setPopulation(103000);
-        country.setCapital("Oranjestad");
 
         extractedCountries.add(country);
         cotyr.displayCountries(extractedCountries, "World", "", true);
@@ -98,13 +95,6 @@ public class UnitTestApp {
     @Test
     void displayTopCountryInContinentTest() {
         ArrayList<Country> extractedCountries = new ArrayList<>();
-        Country country = new Country();
-        country.setCountryCode("ABW");
-        country.setCountryName("Aruba");
-        country.setContinent("North America");
-        country.setRegion("Caribbean");
-        country.setPopulation(103000);
-        country.setCapital("Oranjestad");
 
         extractedCountries.add(country);
         cotyr.displayCountries(extractedCountries, "Continent", "Europe", true);
@@ -113,17 +103,194 @@ public class UnitTestApp {
     @Test
     void displayTopCountryInRegionTest() {
         ArrayList<Country> extractedCountries = new ArrayList<>();
-        Country country = new Country();
-        country.setCountryCode("ABW");
-        country.setCountryName("Aruba");
-        country.setContinent("North America");
-        country.setRegion("Caribbean");
-        country.setPopulation(103000);
-        country.setCapital("Oranjestad");
 
         extractedCountries.add(country);
         cotyr.displayCountries(extractedCountries, "Region", "Eastern Africa", true);
     }
+
+
+
+
+
+
+    @Test
+    void displayCityNullTest(){
+
+        cr.displayCities(null, "World", "", false);
+    }
+
+    @Test
+    void displayCityEmptyTest(){
+        ArrayList<City> extractedCities = new ArrayList<>();
+        cr.displayCities(extractedCities, "World", "", false);
+    }
+
+    @Test
+    void displayCityContainsNullTest(){
+        ArrayList<City> extractedCities = new ArrayList<>();
+        extractedCities.add(null);
+        extractedCities.add(null);
+
+        cr.displayCities(extractedCities, "World", "", false);
+    }
+
+    @Test
+    void displayCityInWorldTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "World", "", false);
+    }
+
+    @Test
+    void displayCityInContinentTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "Continent", "Europe", false);
+    }
+
+    @Test
+    void displayCityInRegionTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "Region", "Eastern Africa", false);
+    }
+
+    @Test
+    void displayCityInCountryTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "Country", "Eastern Africa", false);
+    }
+
+    @Test
+    void displayCityInDistrictTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "District", "Eastern Africa", false);
+    }
+
+    @Test
+    void displayTopCityInWorldTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "World", "", true);
+    }
+
+    @Test
+    void displayTopCityInContinentTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "Continent", "Europe", true);
+    }
+
+    @Test
+    void displayTopCityInRegionTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "Region", "Eastern Africa", true);
+    }
+
+    @Test
+    void displayTopCityInCountryTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "Country", "Eastern Africa", true);
+    }
+
+    @Test
+    void displayTopCityInDistrictTest() {
+        ArrayList<City> extractedCities = new ArrayList<>();
+
+        extractedCities.add(city);
+        cr.displayCities(extractedCities, "District", "Eastern Africa", true);
+    }
+
+
+
+
+
+
+
+
+    @Test
+    void displayCapitalCityNullTest(){
+
+        ccr.displayCapitalCities(null, "World", "", false);
+    }
+
+    @Test
+    void displayCapitalCityEmptyTest(){
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+        ccr.displayCapitalCities(extractedCapitalCities, "World", "", false);
+    }
+
+    @Test
+    void displayCapitalCityContainsNullTest(){
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+        extractedCapitalCities.add(null);
+        extractedCapitalCities.add(null);
+
+        ccr.displayCapitalCities(extractedCapitalCities, "World", "", false);
+    }
+
+    @Test
+    void displayCapitalCityInWorldTest() {
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+
+        extractedCapitalCities.add(city);
+        ccr.displayCapitalCities(extractedCapitalCities, "World", "", false);
+    }
+
+    @Test
+    void displayCapitalCityInContinentTest() {
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+
+        extractedCapitalCities.add(city);
+        ccr.displayCapitalCities(extractedCapitalCities, "Continent", "Europe", false);
+    }
+
+    @Test
+    void displayCapitalCityInRegionTest() {
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+
+        extractedCapitalCities.add(city);
+        ccr.displayCapitalCities(extractedCapitalCities, "Region", "Eastern Africa", false);
+    }
+
+    @Test
+    void displayTopCapitalCityInWorldTest() {
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+
+        extractedCapitalCities.add(city);
+        ccr.displayCapitalCities(extractedCapitalCities, "World", "", true);
+    }
+
+    @Test
+    void displayTopCapitalCityInContinentTest() {
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+
+        extractedCapitalCities.add(city);
+        ccr.displayCapitalCities(extractedCapitalCities, "Continent", "Europe", true);
+    }
+
+    @Test
+    void displayTopCapitalCityInRegionTest() {
+        ArrayList<City> extractedCapitalCities = new ArrayList<>();
+
+        extractedCapitalCities.add(city);
+        ccr.displayCapitalCities(extractedCapitalCities, "Region", "Eastern Africa", true);
+    }
+
+
 
 
 }
