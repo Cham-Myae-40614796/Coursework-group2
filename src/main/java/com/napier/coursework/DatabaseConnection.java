3910779a5568d7749e59b6918aa9199643d04262
thar-htet-nyan <40614795@live.napier.ac.uk>
2023-07-19 22:15:07 +0630

    Reduced Delay in Database

--- src/main/java/com/napier/coursework/DatabaseConnection.java
+++ src/main/java/com/napier/coursework/DatabaseConnection.java
@@ -52,7 +52,7 @@ public class DatabaseConnection {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(10000);
+                Thread.sleep(1000);
                 // Connect to database
                 conn = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
