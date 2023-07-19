74d0720cd808c67165e32c72090811422a632d9d
thar-htet-nyan <138425363+thar-htet-nyan@users.noreply.github.com>
2023-07-19 22:37:16 +0630

    Update DatabaseConnection.java

--- src/main/java/com/napier/coursework/DatabaseConnection.java
+++ src/main/java/com/napier/coursework/DatabaseConnection.java
@@ -52,7 +52,7 @@ public class DatabaseConnection {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(1000);
+                Thread.sleep(10000);
                 // Connect to database
                 conn = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
