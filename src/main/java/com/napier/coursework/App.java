cf9ddedeff3f9743e69f7b002a4572397fdf3969
thar-htet-nyan <40614795@live.napier.ac.uk>
2023-07-27 12:42:45 +0630

    fixed database connection

--- src/main/java/com/napier/coursework/App.java
+++ src/main/java/com/napier/coursework/App.java
@@ -26,7 +26,7 @@ public class App {
         // Create a DatabaseConnection Object
         DatabaseConnection dbConn = new DatabaseConnection();
         // Make connection to database
-        dbConn.connect("localhost", 33061);
+        dbConn.connect("db", 3306);
 
 
         // Create a CountryReport Object
